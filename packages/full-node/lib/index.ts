c4e2872f09c7a15ee195fa4242d8d00277410e8b
Garfield is on board <GarfDev.13@gmail.com>
2021-05-17 11:48:05 +0700

    feat: Change the stack into REST

--- packages/full-node/lib/index.ts
+++ packages/full-node/lib/index.ts
@@ -1,16 +1,1 @@
-import path from "path";
-import Mali, { Context } from "mali";
-
-const PROTO_PATH = path.resolve(__dirname, "../lib/protos/helloworld.proto");
-
-async function sayHello(ctx: Context<any>) {
-  ctx.res = { message: "Hello ".concat(ctx.req.name) };
-}
-
-function main() {
-  const app = new Mali(PROTO_PATH, "Greeter");
-  app.use({ sayHello });
-  app.start("127.0.0.1:50051");
-}
-
-main();
+console.log("Hello world");
