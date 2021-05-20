dc7bd3f883cbd86a7ef1d688db3c55381db3954d
Garfield is on board <GarfDev.13@gmail.com>
2021-05-20 15:25:46 +0700

    Update full-node

--- packages/full-node/app/index.ts
+++ packages/full-node/app/index.ts
@@ -1,5 +1,11 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 function application() {
-  console.log('On construction');
+  const app = express();
+
+  app.listen(process.env.PORT);
 }
 
 application();
